36968e8864a14bc8084f85a3f78f0c6292acf02d
stressGC <g.cosson@live.fr>
2019-07-08 04:03:29 +0200

    migrate all typeguards

---
+++ lib-typeGuards.ts
@@ -0,0 +1,9 @@
+/* type guards */
+export const isNull = (elem: any): elem is null => (null === elem);
+export const isElement = (elem: any): elem is HTMLElement => (elem instanceof HTMLElement);
+export const isHTMLListElement = (elem: any): elem is HTMLLIElement => (elem instanceof HTMLLIElement);
+export const isHTMLAnchorElement = (elem: any): elem is HTMLAnchorElement => (elem instanceof HTMLAnchorElement);
+export const isHTMLDivElement = (elem: any): elem is HTMLDivElement => (elem instanceof HTMLDivElement);
+export const isHTMLImageElement = (elem: any): elem is HTMLImageElement => (elem instanceof HTMLImageElement);
+export const isHTMLSpanElement = (elem: any): elem is HTMLSpanElement => (elem instanceof HTMLSpanElement);
+export const isHTMLFrameElement = (elem: any): elem is HTMLFrameElement => (elem instanceof HTMLFrameElement);
